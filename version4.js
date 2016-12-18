ff41c93a81f0bae677bced3f53997959a721bace
Nick Hughes <nick.hughes@objective.com>
2016-12-18 15:23:57 +1100

    Final commit

--- version4.js
+++ version4.js
@@ -10,14 +10,24 @@ var todoList = {
         });
         this.displayTodos();
     },
-    changeTodo: function(position, newvalue) {
-        this.todos.todoText[position] = newvalue;
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position,1);
         this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
     }
+
 };
 
-displayTodos();
+todoList.addTodo("refactor");
+todoList.changeTodo(0, "updated");
+todoList.toggleCompleted(0);
+//todoList.completeTodo(0);
+//console.log("Test1:", this.todos.completed);
