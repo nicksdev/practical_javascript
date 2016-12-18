1b22ae6626401e6c1abac17365e3d403e3d8a53a
Nick Hughes <nick.hughes@objective.com>
2016-12-18 20:26:47 +1100

    Initial Commit Version 6

---
+++ version6.js
@@ -0,0 +1,48 @@
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log("You have NOTHING to do today - go home!");
+        } else {
+            console.log("My todo's are");
+            for (var i = 0; i < this.todos.length; i++){
+                if (this.todos[i].completed === false){
+                    console.log("( )", this.todos[i].todoText);
+                } else {
+                    console.log("(X)", this.todos[i].todoText);
+                }
+            }
+        }
+    },
+
+
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        //this.displayTodos();
+    },
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        //this.displayTodos();
+    }
+
+};
+
+todoList.addTodo("first");
+todoList.addTodo("second");
+todoList.addTodo("third");
+
+todoList.toggleCompleted(1);
+
+todoList.displayTodos();
